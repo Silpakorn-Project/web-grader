1dbdfe1b3318e3d782d221a6feec52f3926b0cf3
uglypow <uglypow@gmail.com>
2025-01-17 18:50:15 +0700

    chore: install dependencies and add css for react-split

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,48 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './index.css';
+import "./styles/global.css";
+
+// TEMP: Fix resize observer
+const OriginalResizeObserver = window.ResizeObserver;
+
+window.ResizeObserver = function (callback: ResizeObserverCallback) {
+    const wrappedCallback = (entries, observer) => {
+        window.requestAnimationFrame(() => {
+            callback(entries, observer);
+        });
+    };
+
+    return new OriginalResizeObserver(wrappedCallback);
+} as unknown as typeof ResizeObserver;
+
+for (let staticMethod in OriginalResizeObserver) {
+    if (OriginalResizeObserver.hasOwnProperty(staticMethod)) {
+        window.ResizeObserver[staticMethod] =
+            OriginalResizeObserver[staticMethod];
+    }
+}
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+    typography: {
+        fontFamily: `system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, 
+        "Open Sans", "Helvetica Neue", sans-serif`,
+    },
+});
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
