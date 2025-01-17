dfd7a19ec86091c4aa428a2287109ded7fc10d47
uglypow <uglypow@gmail.com>
2025-01-17 19:50:33 +0700

    feat: handle on select language

--- src/components/Workspace/Playground.tsx
+++ src/components/Workspace/Playground.tsx
@@ -1,5 +1,6 @@
-import { LANGUAGE } from "@/constants/languages";
+import { CODE_SNIPPETS, LANGUAGE } from "@/constants/languages";
 import { Editor } from "@monaco-editor/react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import {
     AppBar,
     Box,
@@ -25,114 +26,124 @@ const testCases = [
 // NEED REFACTOR
 const Playground: React.FC<PlaygroundProps> = () => {
     const [selectedTestCase, setSelectedTestCase] = useState(testCases[0]);
-    const [language, setLanguage] = useState("java");
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const [language, setLanguage] = useState("java");
+    const [value, setValue] = useState(CODE_SNIPPETS[language]);
 
     const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
     };
 
-    const handleMenuClose = (selectedLanguage?: string) => {
+    const onSelectLanguage = (selectedLanguage?: string) => {
         if (selectedLanguage) {
             setLanguage(selectedLanguage);
+            setValue(CODE_SNIPPETS[selectedLanguage]);
         }
         setAnchorEl(null);
     };
 
     return (
-        <Stack direction="column" bgcolor="#1e1e1e">
-            <AppBar position="static">
-                <Toolbar
-                    variant="dense"
-                    disableGutters
-                    sx={{ minHeight: 25, height: 25 }}
+        <Box className="h-screen">
+            <Stack direction="column" bgcolor="#1e1e1e">
+                <AppBar position="relative">
+                    <Toolbar
+                        variant="dense"
+                        disableGutters
+                        sx={{ minHeight: 40, height: 40, padding: 1 }}
+                    >
+                        <Button
+                            color="inherit"
+                            size="small"
+                            onClick={handleMenuOpen}
+                            endIcon={<ExpandMoreIcon />}
+                            sx={{
+                                fontSize: "0.90rem",
+                                textTransform: "none",
+                            }}
+                        >
+                            {language}
+                        </Button>
+                        <Menu
+                            anchorEl={anchorEl}
+                            open={Boolean(anchorEl)}
+                            onClose={() => onSelectLanguage()}
+                        >
+                            {LANGUAGE.map((lang) => (
+                                <MenuItem
+                                    key={lang}
+                                    selected={lang === language}
+                                    onClick={() => onSelectLanguage(lang)}
+                                    sx={{ fontSize: "0.90rem" }}
+                                >
+                                    {lang}
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Toolbar>
+                </AppBar>
+                <Split
+                    className="h-[calc(100vh-94px)]"
+                    direction="vertical"
+                    sizes={[60, 40]}
+                    minSize={60}
                 >
-                    <Button
-                        color="inherit"
-                        size="small"
-                        onClick={handleMenuOpen}
-                        sx={{
-                            fontSize: "1.0rem",
-                            textTransform: "none",
-                        }}
-                    >
-                        {language}
+                    <Box>
+                        <Editor
+                            theme="vs-dark"
+                            language={language}
+                            value={value}
+                            defaultValue={CODE_SNIPPETS[language]}
+                            onChange={(value) => setValue(value)}
+                        />
+                    </Box>
+
+                    <Stack direction="column" spacing={2} p={2} overflow="auto">
+                        <Stack direction="row" spacing={2}>
+                            {testCases.map((testCase) => (
+                                <Button
+                                    key={testCase.id}
+                                    color="inherit"
+                                    variant={
+                                        selectedTestCase.id === testCase.id
+                                            ? "contained"
+                                            : "text"
+                                    }
+                                    onClick={() =>
+                                        setSelectedTestCase(testCase)
+                                    }
+                                >
+                                    Case {testCase.id}
+                                </Button>
+                            ))}
+                        </Stack>
+                        <Stack spacing={2}>
+                            <Typography variant="subtitle1" gutterBottom>
+                                Input
+                            </Typography>
+                            <Paper sx={{ backgroundColor: "#484444" }}>
+                                <Box p={2}>{selectedTestCase.input}</Box>
+                            </Paper>
+                            <Typography variant="subtitle1" gutterBottom>
+                                Output
+                            </Typography>
+                            <Paper sx={{ backgroundColor: "#484444" }}>
+                                <Box p={2}>{selectedTestCase.output}</Box>
+                            </Paper>
+                        </Stack>
+                    </Stack>
+                </Split>
+
+                <Box display="flex" p={2} gap={2}>
+                    <Button variant="contained" color="inherit" size="small">
+                        Run
                     </Button>
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={Boolean(anchorEl)}
-                        onClose={() => handleMenuClose()}
-                        PaperProps={{
-                            sx: { mt: 1 },
-                        }}
-                    >
-                        {LANGUAGE.map((lang) => (
-                            <MenuItem
-                                key={lang}
-                                selected={lang === language}
-                                onClick={() => handleMenuClose(lang)}
-                                sx={{ fontSize: "0.75rem" }}
-                            >
-                                {lang}
-                            </MenuItem>
-                        ))}
-                    </Menu>
-                </Toolbar>
-            </AppBar>
-            <Split
-                className="h-[calc(100vh-94px)]"
-                direction="vertical"
-                sizes={[60, 40]}
-                minSize={60}
-            >
-                <Box>
-                    <Editor theme="vs-dark" />
+                    <Button variant="contained" color="success" size="medium">
+                        Submit
+                    </Button>
                 </Box>
-
-                <Stack direction="column" spacing={2} p={2} overflow="auto">
-                    <Stack direction="row" spacing={2}>
-                        {testCases.map((testCase) => (
-                            <Button
-                                key={testCase.id}
-                                color="inherit"
-                                variant={
-                                    selectedTestCase.id === testCase.id
-                                        ? "contained"
-                                        : "text"
-                                }
-                                onClick={() => setSelectedTestCase(testCase)}
-                            >
-                                Case {testCase.id}
-                            </Button>
-                        ))}
-                    </Stack>
-                    <Stack spacing={2}>
-                        <Typography variant="subtitle1" gutterBottom>
-                            Input
-                        </Typography>
-                        <Paper elevation={-2}>
-                            <Box p={2}>{selectedTestCase.input}</Box>
-                        </Paper>
-                        <Typography variant="subtitle1" gutterBottom>
-                            Output
-                        </Typography>
-                        <Paper elevation={-2}>
-                            <Box p={2}>{selectedTestCase.output}</Box>
-                        </Paper>
-                    </Stack>
-                </Stack>
-                
-            </Split>
-
-            <Box display="flex" p={2} gap={2}>
-                <Button variant="contained" color="inherit" size="small">
-                    Run
-                </Button>
-                <Button variant="contained" color="success" size="medium">
-                    Submit
-                </Button>
-            </Box>
-        </Stack>
+            </Stack>
+        </Box>
     );
 };
 
