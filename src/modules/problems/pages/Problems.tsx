ced77bb8fd0b92e499d08950f9e0ebd3e348fc29
Puriwat <uglypow@gmail.com>
2025-04-07 15:21:32 +0700

    feat: problems table page with server side pagination filtering and sorting

--- src/modules/problems/pages/Problems.tsx
+++ src/modules/problems/pages/Problems.tsx
@@ -1,59 +1,251 @@
-import ProblemTable from "@/modules/problems/components/ProblemTable";
+import { useDebounce } from "@/hooks/useDebounce";
 import { client } from "@/services";
-import { Box, CircularProgress, Typography } from "@mui/material";
+import { getDifficultyColor } from "@/utilts/common";
+import SearchIcon from "@mui/icons-material/Search";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import {
+    Button,
+    Chip,
+    Container,
+    FormControl,
+    Input,
+    InputAdornment,
+    Stack,
+    styled,
+    Typography,
+} from "@mui/material";
+import {
+    DataGrid,
+    GridColDef,
+    GridPaginationModel,
+    GridSortModel,
+} from "@mui/x-data-grid";
 import { useQuery } from "@tanstack/react-query";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ProblemsFilter from "../components/ProblemsFilter";
 
-type ProblemPageProps = {};
+const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
+    cursor: "pointer",
+    "& .MuiDataGrid-cell:focus": {
+        outline: "none",
+    },
+    "& .MuiDataGrid-cell:focus-within": {
+        outline: "none",
+    },
+    "&, [class^=MuiDataGrid]": { border: "none" },
+    "& .MuiDataGrid-row:nth-of-type(even)": {
+        backgroundColor: theme.palette.background.default,
+    },
+    "& .MuiDataGrid-row:nth-of-type(odd)": {
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
 
-const Problems: FC<ProblemPageProps> = () => {
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ["problems"],
+type ProblemTableProps = {};
+
+const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", flex: 1, disableColumnMenu: true },
+    {
+        field: "difficulty",
+        headerName: "Difficulty",
+        flex: 0.7,
+        disableColumnMenu: true,
+        renderCell: (params) => (
+            <Chip
+                label={params.value}
+                color={getDifficultyColor(params.value)}
+                variant="outlined"
+            />
+        ),
+    },
+    { field: "type", headerName: "Type", flex: 0.7, disableColumnMenu: true },
+];
+
+const Problems: FC<ProblemTableProps> = () => {
+    const navigate = useNavigate();
+    const [filters, setFilters] = useState<{
+        difficulty?: string;
+        type?: string;
+    }>({});
+    const [searchQuery, setSearchQuery] = useState("");
+    const [paginationModel, setPaginationModel] = useState({
+        page: 0,
+        pageSize: 5,
+    });
+    const [rowCount, setRowCount] = useState(0);
+    const [sortModel, setSortModel] = useState<{
+        field: string;
+        sort: "asc" | "desc";
+    } | null>(null);
+
+    const debouncedSearch = useDebounce(searchQuery);
+
+    const {
+        data: problems,
+        isFetching,
+        isLoading,
+    } = useQuery({
+        queryKey: [
+            "problems",
+            { filters, paginationModel, debouncedSearch, sortModel },
+        ],
         queryFn: async () => {
-            const response = await client.graderService.problems.getProblems();
-            return response.data;
+            return await client.graderService.problems.getProblems({
+                title: debouncedSearch,
+                type: filters.type,
+                difficulty: filters.difficulty,
+                offset: paginationModel.page + 1,
+                limit: paginationModel.pageSize,
+                sortBy: sortModel?.field,
+                sortType: sortModel?.sort,
+            });
         },
+        placeholderData: (prev) => prev,
     });
 
-    if (isLoading || !data)
-        return (
-            <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                height="100vh"
-            >
-                <CircularProgress />
-            </Box>
-        );
+    useEffect(() => {
+        if (problems) {
+            setRowCount(problems.totalRecords || 0);
+        }
+    }, [problems]);
 
-    if (isError)
-        return (
-            <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                height="100vh"
-            >
-                <Typography color="error">Error loading problems.</Typography>
-            </Box>
-        );
+    const handleRandomProblem = () => {
+        if (problems && problems.data.length > 0) {
+            const randomIndex = Math.floor(
+                Math.random() * problems.data.length
+            );
+            navigate(`/problems/${problems.data[randomIndex].problemId}`);
+        }
+    };
+
+    const handlePaginationModelChange = (
+        newPaginationModel: GridPaginationModel
+    ) => {
+        setPaginationModel(newPaginationModel);
+    };
+
+    const handleSortModelChange = (newSortModel: GridSortModel) => {
+        if (newSortModel.length > 0) {
+            const { field, sort } = newSortModel[0];
+
+            if (field && sort) {
+                setSortModel({ field, sort });
+            }
+        } else {
+            setSortModel(null);
+        }
+    };
+
+    const handleFilterChange = (newFilters: {
+        difficulty?: string;
+        type?: string;
+    }) => {
+        setFilters(newFilters);
+    };
+
+    const handleRemoveFilter = (filterKey: keyof typeof filters) => {
+        setFilters((prev) => {
+            const updatedFilters = { ...prev };
+            delete updatedFilters[filterKey];
+            return updatedFilters;
+        });
+    };
 
     return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="start"
-            p={3}
-        >
-            <Typography variant="h4">Problem List</Typography>
-            <Typography variant="subtitle1">
-                Browse through the list of problems and select one to start
-                solving.
-            </Typography>
-            <ProblemTable problems={data} />
-        </Box>
+        <Container maxWidth="lg">
+            <Stack
+                display="flex"
+                direction="row"
+                justifyContent="end"
+                alignItems="center"
+                gap={2}
+                my={4}
+            >
+                <ProblemsFilter
+                    onChange={handleFilterChange}
+                    selectedFilters={filters}
+                />
+
+                <FormControl variant="standard" size="small">
+                    <Input
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        }
+                        placeholder="Search"
+                    />
+                </FormControl>
+                <Button
+                    size="small"
+                    variant="contained"
+                    color="success"
+                    onClick={handleRandomProblem}
+                    startIcon={<ShuffleIcon sx={{ color: "white" }} />}
+                    sx={{ cursor: "pointer", display: "inline-flex" }}
+                >
+                    <Typography sx={{ color: "white" }} variant="subtitle2">
+                        Random
+                    </Typography>
+                </Button>
+                {/* <Button
+                    size="small"
+                    variant="contained"
+                    color="primary"
+                    onClick={() => navigate("/admin/create")}
+                    startIcon={<AddIcon sx={{ color: "white" }} />}
+                    sx={{ cursor: "pointer", display: "inline-flex" }}
+                >
+                    <Typography sx={{ color: "white" }} variant="subtitle2">
+                        Create
+                    </Typography>
+                </Button> */}
+            </Stack>
+
+            <Stack direction="row" spacing={1} my={2}>
+                {Object.entries(filters).map(([key, value]) =>
+                    value ? (
+                        <Chip
+                            key={key}
+                            label={value}
+                            onDelete={() =>
+                                handleRemoveFilter(key as keyof typeof filters)
+                            }
+                            variant="filled"
+                        />
+                    ) : null
+                )}
+            </Stack>
+
+            <StyledDataGrid
+                rows={
+                    problems
+                        ? problems.data.map((p) => ({ ...p, id: p.problemId }))
+                        : []
+                }
+                loading={isLoading}
+                columns={columns}
+                paginationMode="server"
+                sortingMode="server"
+                rowCount={rowCount}
+                pageSizeOptions={[5, 10, 20, 100]}
+                paginationModel={paginationModel}
+                onPaginationModelChange={handlePaginationModelChange}
+                onSortModelChange={handleSortModelChange}
+                disableRowSelectionOnClick
+                disableColumnResize
+                onRowClick={(params) => navigate(`/problems/${params.id}`)}
+                sx={{
+                    cursor: isFetching ? "not-allowed" : "pointer",
+                    opacity: isFetching ? 0.5 : 1,
+                    pointerEvents: isFetching ? "none" : "auto",
+                    transition: "opacity 0.3s ease",
+                }}
+            />
+        </Container>
     );
 };
 
