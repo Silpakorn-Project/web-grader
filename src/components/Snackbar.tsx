558729829c51c68a0cc8eddc50fb6fdc84654ccb
Puriwat <uglypow@gmail.com>
2025-04-16 19:05:10 +0700

    fix: snackbar not updated on position in store

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -2,16 +2,24 @@ import { useSnackbarStore } from "@/store/SnackbarStore";
 import { Alert, Snackbar } from "@mui/material";
 
 const GlobalSnackbar = () => {
-    const { open, message, severity, closeSnackbar } = useSnackbarStore();
+    const { open, message, severity, position, closeSnackbar } =
+        useSnackbarStore();
 
     return (
         <Snackbar
-            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            anchorOrigin={{
+                vertical: position.vertical,
+                horizontal: position.horizontal,
+            }}
             open={open}
             autoHideDuration={6000}
             onClose={closeSnackbar}
         >
-            <Alert onClose={closeSnackbar} severity={severity} sx={{ width: "100%" }}>
+            <Alert
+                onClose={closeSnackbar}
+                severity={severity}
+                sx={{ width: "100%" }}
+            >
                 {message}
             </Alert>
         </Snackbar>
