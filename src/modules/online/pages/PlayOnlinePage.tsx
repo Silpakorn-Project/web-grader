25984da35771a5ee63057bccfd36ab15c177ba1f
sodtiruk <sodtiruk22@gmail.com>
2025-04-18 06:29:31 +0700

    refactor: remove file unused

--- src/modules/online/pages/PlayOnlinePage.tsx
+++ src/modules/online/pages/PlayOnlinePage.tsx
@@ -1,21 +1,6 @@
 import Workspace from "@/modules/workspace/pages/Workspace";
-import { useSocketStore } from "@/store/SocketStore";
-import { useEffect, useRef } from "react";
 
 const PlayOnlinePage: React.FC = () => {
-    const { disconnectSocket } = useSocketStore();
-    const mounted = useRef(false);
-
-    useEffect(() => {
-        //fix strict mode when you use developer mode because mount unmount twice
-        // and then you will disconnect socket but i don't want to disconnect socket
-
-        if (!mounted.current) {
-            mounted.current = true;
-            return;
-        }
-    }, []);
-
     return <Workspace />;
 };
 
